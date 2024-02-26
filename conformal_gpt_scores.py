8da8806f8bd10028d51ae4ae5839f4857a982cb6
Katherine <krosenf@gmail.com>
2024-02-26 18:15:52 +0000

    add task list

---
+++ conformal_gpt_scores.py
@@ -0,0 +1,15 @@
+"""
+GPT version of conformal_llm_scores.py
+"""
+
+# List of task we consider
+task_list = ['college_computer_science', 'formal_logic', 'high_school_computer_science',
+             'computer_security', 'machine_learning',
+
+             'clinical_knowledge', 'high_school_biology', 'anatomy', 'college_chemistry',
+             'college_medicine', 'professional_medicine',
+
+             'business_ethics', 'professional_accounting', 'public_relations',
+             'management', 'marketing'
+             ]
+
